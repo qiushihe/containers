449a6f4fad7b14ad26c6f4f1c6b7e1227fd8e22a
Billy He <qiushihe@me.com>
2017-03-22 14:59:18 -0700

    Add certbot support to domain proxy

--- domain-proxy/src/run-domain-proxy.sh
+++ domain-proxy/src/run-domain-proxy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+useCertbot=$DOMAIN_PROXY_USE_CERTBOT
+boulderHost=$DOMAIN_PROXY_BOULDER_HOST
+
 rm -fr /nginx-configs/*
 DOMAIN_FES=$(compgen -A variable | grep -e "^DOMAIN_PROXY_FE_[^_]\+$")
 for fe in $DOMAIN_FES; do
@@ -13,10 +16,16 @@ for fe in $DOMAIN_FES; do
   feHost="${feUrlParts[0]}"
   fePort="${feUrlParts[1]}"
   feSsl=""
-  if [ "$fePort" == "443" ] && [ ! -d "/certs/$feHost" ]; then
-    /create-self-signed-cert.sh $feHost
-    feSsl="/certs/$feHost"
-    echo "!!! Created self-signed certificate for $feHost"
+  if [ "$fePort" == "443" ]; then
+    if [ -n "$useCertbot" ]; then
+      /request-certbot-cert.sh $feHost $boulderHost
+      feSsl="/etc/letsencrypt/live/$feHost"
+      echo "!!! Got certificate from certbot for $feHost"
+    else
+      /create-self-signed-cert.sh $feHost
+      feSsl="/certs/$feHost"
+      echo "!!! Created self-signed certificate for $feHost"
+    fi
   fi
 
   # Create proxy config
