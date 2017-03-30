820009efda63f02f1875eec5b8f4cf21c75d7702
Billy He <qiushihe@me.com>
2017-03-29 17:55:41 -0700

    Update NFS test client

--- nfs-server-client/src/run-nfs-server-client.sh
+++ nfs-server-client/src/run-nfs-server-client.sh
@@ -2,11 +2,24 @@
 
 echo "!!! Starting NFS server client"
 
+wait_tcp_port() {
+  local host="$1" port="$2"
+  while ! exec 6<>/dev/tcp/$host/$port; do
+    echo "$(date) - still trying to connect to $host:$port"
+    sleep 5
+  done
+  exec 6>&-
+}
+
 rpcbind -f &
 echo "!!! Started rpcbind"
 
-# mkdir -p /mnt/lala1
-# mount -v -t nfs -o proto=tcp,port=2049,vers=3 nfs-server:/exports/lala1 /mnt/lala1
+wait_tcp_port nfs-server 2049
+echo "!!! nfs-server:2049 is ready"
+
+mkdir -p /mnt/lala1
+mount -v -t nfs -o proto=tcp,port=2049,vers=3 nfs-server:/exports/lala1 /mnt/lala1
+echo "!!! Mounted /mnt/lala1"
 
 while true; do
   sleep 5
