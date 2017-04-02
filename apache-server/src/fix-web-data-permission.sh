32c637d8c77611dc0a68b7843e69a747c9bb72ee
Billy He <qiushihe@me.com>
2017-04-02 12:01:22 -0700

    Fix web data permission issue with php-fpm running under nobody

---
+++ apache-server/src/fix-web-data-permission.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+chown -R apache:apache /web-data/www
+chmod -R 775 /web-data/www
--- apache-server/src/run-apache-server.sh
+++ apache-server/src/run-apache-server.sh
@@ -2,9 +2,4 @@
 
 echo "!!! Starting apache server"
 
-if [ ! -d "/web-data/www" ]; then
-  mkdir /web-data/www
-  echo "<?php phpinfo(); ?>" > /web-data/www/index.php
-fi
-
 supervisord -c /supervisord.conf
