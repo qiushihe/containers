6b5a7d2dc77edada063f64f7ec9db9b4066c4280
Billy He <qiushihe@me.com>
2017-05-10 21:45:52 -0700

    WIP Traffic boulder integration

--- traefik-server/src/entry.sh
+++ traefik-server/src/entry.sh
@@ -3,5 +3,12 @@
 echo "!!! Rebuild Traefik Rules"
 /utilities/update-rules.sh
 
+# caServer="https://acme-staging.api.letsencrypt.org/directory"
+# echo "!!! Set Traefik CA Server URL to $caServer"
+# sed -i "s|TRAEFIK-ACME-CA-SERVER-URL|${caServer}|g" /traefik/traefik.toml
+
 echo "!!! Starting Traefik Server"
-traefik -c /traefik/traefik.toml
+#traefik -c /traefik/traefik.toml
+while true; do
+  sleep 5
+done
