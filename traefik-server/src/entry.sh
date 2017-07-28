6d26d1e6fecd8762da7832268f0b9aa7bbd3d061
Billy He <qiushihe@me.com>
2017-07-27 17:45:11 -0700

    Add a wait to traefic startup

--- traefik-server/src/entry.sh
+++ traefik-server/src/entry.sh
@@ -9,5 +9,8 @@ echo "!!! Rebuild Traefik Rules"
 echo "!!! Set Traefik CA Server URL to $traefikServerCaServer"
 sed -i "s|TRAEFIK-ACME-CA-SERVER-URL|${traefikServerCaServer}|g" /traefik/traefik.toml
 
+# TODO: Implement a TCP check instead of waiting for a number of seconds
+sleep 5
+
 echo "!!! Starting Traefik Server"
 traefik -c /traefik/traefik.toml
