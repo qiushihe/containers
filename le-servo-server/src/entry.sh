fd8962ebd398e6d410039a67cb3d07eaf459c76a
Billy He <qiushihe@me.com>
2017-07-30 22:39:30 -0700

    WIP

--- le-servo-server/src/entry.sh
+++ le-servo-server/src/entry.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "!!! Starting LE-Servo Server"
-
 leServoHostname="${LE_SERVO_SERVER_HOST_NAME%\"}"
 leServoHostname="${leServoHostname#\"}"
 leServoPathPrefix="${LE_SERVO_SERVER_PATH_PREFIX%\"}"
@@ -13,11 +11,12 @@ leServoNonceBufferSize="${leServoNonceBufferSize#\"}"
 
 cd /le-servo
 
-LE_SERVO_HOST_NAME=$leServoHostname \
-LE_SERVO_PATH_PREFIX=$leServoPathPrefix \
-LE_SERVO_PORT=$leServoPort \
-LE_SERVO_NONCE_BUFFER_SIZE=$leServoNonceBufferSize \
-npm run server
+# echo "!!! Starting LE-Servo Server"
+# LE_SERVO_HOST_NAME=$leServoHostname \
+# LE_SERVO_PATH_PREFIX=$leServoPathPrefix \
+# LE_SERVO_PORT=$leServoPort \
+# LE_SERVO_NONCE_BUFFER_SIZE=$leServoNonceBufferSize \
+# npm run server
 
 while true; do
   sleep 5
--- traefik-server/src/entry.sh
+++ traefik-server/src/entry.sh
@@ -9,8 +9,11 @@ echo "!!! Rebuild Traefik Rules"
 echo "!!! Set Traefik CA Server URL to $traefikServerCaServer"
 sed -i "s|TRAEFIK-ACME-CA-SERVER-URL|${traefikServerCaServer}|g" /traefik/traefik.toml
 
-# TODO: Implement a TCP check instead of waiting for a number of seconds
-sleep 5
+# echo "!!! Starting Traefik Server"
+# # TODO: Implement a TCP check instead of waiting for a number of seconds
+# sleep 5
+# traefik -c /traefik/traefik.toml
 
-echo "!!! Starting Traefik Server"
-traefik -c /traefik/traefik.toml
+while true; do
+  sleep 5
+done
