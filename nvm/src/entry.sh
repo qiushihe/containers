e6de0875dbe4286f41b7f003e2214a40afabea30
Billy He <billy.he@gmail.com>
2018-05-21 21:36:05 -0700

    Implement nvm image

--- nvm/src/entry.sh
+++ nvm/src/entry.sh
@@ -5,6 +5,9 @@ echo "!!! Starting nvm server"
 export NVM_DIR="/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-while true; do
-  sleep 1
-done
+cd /project
+nvm install
+nvm use
+npm run dev
+
+# while true; do sleep 1; done
